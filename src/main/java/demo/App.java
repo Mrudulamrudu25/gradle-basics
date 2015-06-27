0642e052da315d1308a866d06163b687a7085451
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-06-27 20:56:06 +0200

    install dependencies with gradle

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,8 +1,14 @@
 package demo;
 
+import org.joda.time.LocalTime;
+
 public class App {
 
     public static void main(String... args) {
+
+        LocalTime time = new LocalTime();
+        System.out.println("Current time: " + time);
+
         Greeter greeter = new Greeter();
         greeter.sayHello();
     }
